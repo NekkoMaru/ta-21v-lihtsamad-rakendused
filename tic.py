86a122d9ea4d056c8635c28b3196bb9e5d426943
NekkoMaru <nekkofanu@gmail.com>
2022-03-31 10:58:59 +0300

    Update tic.py

--- tic.py
+++ tic.py
@@ -1,49 +1,49 @@
 next_position = input("Type what position: ")
 
 if next_position == "1":
-print("x . .")
-print(". . .")
-print(". . .")
+    print("x . .")
+    print(". . .")
+    print(". . .")
 
     if next_position == "1":
         print("CANT DO THE MOVE")
 
 if next_position == "2":
-print(". x .")
-print(". . .")
-print(". . .")
+    print(". x .")
+    print(". . .")
+    print(". . .")
 
 if next_position == "3":
-print(". . x")
-print(". . .")
-print(". . .")
+    print(". . x")
+    print(". . .")
+    print(". . .")
 
 if next_position == "4":
-print(". . .")
-print("x . .")
-print(". . .")
+    print(". . .")
+    print("x . .")
+    print(". . .")
 
 if next_position == "5":
-print(". . .")
-print(". x .")
-print(". . .")
+    print(". . .")
+    print(". x .")
+    print(". . .")
 
 if next_position == "6":
-print(". . .")
-print(". . x")
-print(". . .")
+    print(". . .")
+    print(". . x")
+    print(". . .")
 
 if next_position == "7":
-print(". . .")
-print(". . .")
-print("x . .")
+    print(". . .")
+    print(". . .")
+    print("x . .")
 
 if next_position == "8":
-print(". . .")
-print(". . .")
-print(". x .")
+    print(". . .")
+    print(". . .")
+    print(". x .")
 
 if next_position == "9":
-print(". . .")
-print(". . .")
-print(". . x")
+    print(". . .")
+    print(". . .")
+    print(". . x")
